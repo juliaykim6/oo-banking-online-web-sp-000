cf8c23155b43f338c24cdff4364094e064de4ba3
Julia Kim <juliaykim6@gmail.com>
2021-02-15 01:53:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,9 @@
 class BankAccount
-  attr_accessor :name, :balance
-  def initialize(name, balance = 1000)
+  attr_accessor :name, :balance, :status
+
+  def initialize(name, balance = 1000, status = open)
     @name = name
     @balance = balance
+    @status = status
   end
 end
