770a6e44a8662c2f638be34df2e87b1004d430e4
Julia Kim <juliaykim6@gmail.com>
2021-02-15 02:07:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,11 @@
 class Transfer
 
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
-    @amount = amount
+    @amount = amount 
   end
 end
